73d524c1513762a4012a02ceacf183f5198438ae
Johan Postma <jpostma@hotmail.com>
2014-02-22 12:09:04 +0100

    fresh vim install plugin script

--- install_vimplugins.sh
+++ install_vimplugins.sh
@@ -5,7 +5,6 @@ echo "Installing favorite vim plugins"
 InstallVimPlugins() {
 	echo "install now"
 
-<<<<<<< HEAD
 	echo "installing pathogen."
 	mkdir $HOME/.vim/bundle -p
 	mkdir $HOME/.vim/autoload -p
@@ -22,24 +21,6 @@ InstallVimPlugins() {
 	git clone https://github.com/scrooloose/nerdtree.git
 	git clone git://github.com/tpope/vim-commentary.git
 	echo "Done!"
-=======
-	read -p "Do you want to install Pathogen?" -n 1 -r echo
-	if [[  $REPLY =~ ^[Yy]$ ]]
-	then
-		echo "installing pathogen."
-		mkdir ~/.vim/bundle ~/.vim/autoload -p
-		
-		cd ~/.vim/auload
-		curl -Sso $HOME/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-				
-		cd ~/.vim/bundle
-		git clone https://github.com/tomtom/tlib_vim.git
-		git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
-		git clone https://github.com/garbas/vim-snipmate.git
-		git clone https://github.com/honza/vim-snippets.git
-		git clone https://github.com/scrooloose/nerdtree.git
-	fi
->>>>>>> cf10b7d1fe0f76c9417cbdb1be6d96f09321f5bf
 }
 
 InstallVimPlugins
