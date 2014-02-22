03c26ee7beedd848f0a61bda9c478c866b383905
Johan Postma <jpostma@hotmail.com>
2014-02-22 12:01:29 +0100

    install script improvments

--- install_vimplugins.sh
+++ install_vimplugins.sh
@@ -5,33 +5,22 @@ echo "Installing favorite vim plugins"
 InstallVimPlugins() {
 	echo "install now"
 
-	read -p "Do you want to install Pathogen?" -n 1 -r echo
-	if [[  $REPLY =~ ^[Yy]$ ]]
-	then
-		echo "installing pathogen."
-		mkdir ~/.vim/bundle ~/.vim/autoload -p
-		
-		cd ~/.vim/auload
-		curl -Sso $HOME/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-				
-		cd ~/.vim/bundle
-		git clone https://github.com/tomtom/tlib_vim.git
-		git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
-		git clone https://github.com/garbas/vim-snipmate.git
-		git clone https://github.com/honza/vim-snippets.git
-		git clone https://github.com/scrooloose/nerdtree.git
-	fi
+	echo "installing pathogen."
+	mkdir $HOME/.vim/bundle -p
+	mkdir $HOME/.vim/autoload -p
 
-	read -p "Do you want to install NERDtree?" -n 1 -r echo
-	if [[  $REPLY =~ ^[Yy]$ ]]
-	then
-		echo "installing NERDtree."
-	fi	
+	cd $HOME/.vim/autoload
+	curl -Sso $HOME/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
-
-
-
-
+	echo "Installing lots of other plugins..."
+	cd $HOME/.vim/bundle
+	git clone https://github.com/tomtom/tlib_vim.git
+	git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+	git clone https://github.com/garbas/vim-snipmate.git
+	git clone https://github.com/honza/vim-snippets.git
+	git clone https://github.com/scrooloose/nerdtree.git
+	git clone git://github.com/tpope/vim-commentary.git
+	echo "Done!"
 }
 
 InstallVimPlugins
